edaa04c2a6d51e06296e7bf57cdefe92e1d6add6
AdrienTurmo <adrien.turmo@gmail.com>
2020-05-03 02:12:34 +0200

    Spin du résultat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,27 @@ export class AppComponent {
   }
 
   spinTheWheel() {
-    this.result = this.spinTheWheelService.chooseForMe(this.list);
+    this.rapidSpin(50, 20);
+    this.rapidSpin(40, 40);
+    this.rapidSpin(30, 100);
+    this.rapidSpin(20, 200);
+    this.rapidSpin(10, 400);
+    this.rapidSpin(5, 500);
+    this.rapidSpin(5, 700);
+    this.rapidSpin(4, 1000);
   }
 
+  private rapidSpin(maxTurn = 50, ms = 20) {
+    let turn1;
+    turn1 = 0;
+    let spin1;
+    spin1 = setInterval(() => {
+      if (turn1 <= maxTurn) {
+        this.result = this.spinTheWheelService.chooseForMe(this.list);
+        turn1++;
+      } else {
+        clearInterval(spin1);
+      }
+    }, ms);
+  }
 }
