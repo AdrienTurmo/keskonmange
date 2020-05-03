9b349e9bed39d9102e390a5abbada5b876cb00e8
AdrienTurmo <adrien.turmo@gmail.com>
2020-05-03 20:17:44 +0200

    WIP tests

--- src/app/bouffe-item.spec.ts
+++ src/app/bouffe-item.spec.ts
@@ -1,7 +1,7 @@
 import {BouffeItem} from './bouffe-item';
 
 describe('BouffeItem', () => {
-  it('should create an instance', () => {
-    expect(new BouffeItem()).toBeTruthy();
-  });
+  // it('should create an instance', () => {
+  //   expect(new BouffeItem()).toBeTruthy();
+  // });
 });
